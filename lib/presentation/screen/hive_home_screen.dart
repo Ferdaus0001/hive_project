1c2bd160bb6b7937e399524aaffe763185e51307
ferdaus0001 <ferdaushossan7788990@gmail.com>
2025-04-16 22:29:24 +0600

    hive project

---
+++ lib/presentation/screen/hive_home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class HiveHomeScreen extends StatefulWidget {
+  const HiveHomeScreen({super.key});
+
+  @override
+  State<HiveHomeScreen> createState() => _HiveHomeScreenState();
+}
+
+class _HiveHomeScreenState extends State<HiveHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
