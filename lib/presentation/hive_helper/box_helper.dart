9963e3527728aff960cddfe7f894ab4515949642
ferdaus0001 <ferdaushossan7788990@gmail.com>
2025-04-19 21:52:15 +0600

    add new data

--- lib/presentation/hive_helper/box_helper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:hive_project/presentation/hive_helper/notes_models.dart';
-
-class Boxes {
-  static Box<NotesModel> getData() => Hive.box<NotesModel>('notes');
-}
